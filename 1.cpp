cbdaeff34baec23b6e7b92dd4dfe18e402b67a38
AryaShrivastava05 <65621525+AryaShrivastava05@users.noreply.github.com>
2020-10-03 19:48:40 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,40 +2,40 @@
 
 using namespace std;
 
-int insert(int arr[], int n, int item, int pos)
+int insert(int arr[], int N, int item, int pos)
 {
     int i;
     //increase size of Array
-    n++; 
+    N++; 
     //shift elements
-    for(i=n; i>=pos;i--)
+    for(i=N; i>=pos;i--)
         arr[i] = arr[i-1];
     //inserting item
     arr[pos-1] = item;
     cout<<"Updated Array::";
-    for(i=0;i<n;i++)
+    for(i=0;i<N;i++)
         cout<<arr[i]<<" ";
     
 }
 
 int main()
 {
-    int n,i;
+    int N,i;
     int pos, item;
     cout<<"Enter Size of Array:";
-    cin>>n;
+    cin>>N;
     int * arr = new int;
     cout<<"Enter Elements";
-    for(i=0;i<n;i++)
+    for(i=0;i<N;i++)
         cin>>arr[i];
     cout<<"Enter the new element";
     cin>>item;
     cout<<"Enter Position";
     cin>>pos;
     cout<<"Original Array:";
-    for(i=0;i<n;i++)
+    for(i=0;i<N;i++)
         cout<<arr[i]<<" ";
         cout<<endl;
-    insert(arr, n, item, pos);
+    insert(arr, N, item, pos);
     return 0;
 }
